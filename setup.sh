54f92b6c68da3f7e24e6dca8e313aaf70a084700
koder101 <koder101@users.noreply.github.com>
2024-07-27 22:36:20 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This file is only for a quick setup process in fresh re-install of proxmox
+
+# Add the non production repo
+echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" | sudo tee -a /etc/apt/sources.list
+
+apt update && sudo apt dist-upgrade -y
+
+# Install the Packages
+apt install xfce4 chromium lightdm
+
+adduser bunny
+
+systemctl start lightdm
