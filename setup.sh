1d00845fc079f43b4cf9525e0c2cd4e395399e55
koder101 <koder101@users.noreply.github.com>
2025-01-25 19:26:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # This file is only for a quick setup process in fresh re-install of proxmox
 
+# ----------------------------------------------------------------------------------- #
 # Add the non production repo
 # echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" | sudo tee -a /etc/apt/sources.list
 
@@ -16,8 +17,17 @@
 
 # Start the Desktop
 # systemctl start lightdm
-
+# ----------------------------------------------------------------------------------- #
 
 # https://community-scripts.github.io/ProxmoxVE/scripts?id=post-pve-install
 # https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/post-pve-install.sh
-wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/misc/post-pve-install.sh
+wget -qLO - 
+
+# Download the script from Github
+wget -qLO /tmp/post-pve-install.sh https://github.com/community-scripts/ProxmoxVE/raw/main/misc/post-pve-install.sh
+
+# Make the downloaded script executable
+chmod +x /tmp/post-pve-install.sh
+
+# Run the downloaded script
+/tmp/post-pve-install.sh
