0eca6317028e8313d1c789732204b2a4592b44ec
koder101 <koder101@users.noreply.github.com>
2025-01-25 16:27:46 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,16 +3,21 @@
 # This file is only for a quick setup process in fresh re-install of proxmox
 
 # Add the non production repo
-echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" | sudo tee -a /etc/apt/sources.list
+# echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" | sudo tee -a /etc/apt/sources.list
 
-apt update && sudo apt dist-upgrade -y
+# apt update && sudo apt dist-upgrade -y
 
 # Install the Packages
-apt install xfce4 chromium lightdm -y
+# apt install xfce4 chromium lightdm -y
 
 # Prompt for username
-read -p "Enter username: " username
-adduser "$username"
+# read -p "Enter username: " username
+# adduser "$username"
 
 # Start the Desktop
-systemctl start lightdm
+# systemctl start lightdm
+
+
+# https://community-scripts.github.io/ProxmoxVE/scripts?id=post-pve-install
+# https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/post-pve-install.sh
+wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/misc/post-pve-install.sh
